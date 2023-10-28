927dd894e13898e00dc8439ab07972aad0d93cce
nocive.science <nocive.science@gmail.com>
2023-10-27 22:54:06 -0300

    quedo un enrredo aca

--- script.js
+++ script.js
@@ -195,6 +195,39 @@ function main(timestamp) {
             const velocityChangeY = accelerationY * timeElapsed;
             const frictionDeltaX = frictionX * timeElapsed;
             const frictionDeltaY = frictionY * timeElapsed;
+            balls.forEach((ball)=>{
+                if(velocityChangeX === 0){
+                    ball.velocityX = slow(ball.velocityX, frictionDeltaX);
+                }else{
+                    ball.velocityX += velocityChangeX;
+                    ball.velocityX = Math.minmax(ball.velocityX, 1.5);
+                    ball.velocityX = ball.velocityX - Math.sign(velocityChangeX) * frictionDeltaX;
+                    ball.velocityX = Math.minmax(ball.velocityX, maxVelocity);
+                }
+                if(velocityChangeY === 0){
+                    ball.velocityY = slow(ball.velocityY, frictionDeltaY);
+                }else{
+                    ball.velocityY += velocityChangeY;
+                    // ball.velocityY = Math.minmax(ball.velocityY, maxVelocity);
+                    ball.velocityY = ball.velocityY - Math.sign(velocityChangeY) * frictionDeltaY;
+                    ball.velocityY = Math.minmax(ball.velocityY, maxVelocity);
+                };
+                ball.nextX = ball.x + ball.velocityX;
+                ball.nextY = ball.y + ball.velocityY;
+                // ballElements.forEach((ballElement, index) => {
+                //     ballElement.style.cssText = `}
+                //         left: ${ball.x}px;
+                //         top: ${ball.y}px;
+                //     `;
+                // });
+                walls.forEach((wall, wi) => {
+                    if(wall.horizontal){
+                        if(ball.nextY+ballSize/2>=wall.y-wallW/2 && ball.nextY-ballSize/2<=wall.y+wallW/2){
+                            console.log(ball.nextY+ballSize/2>=wall.y-wallW/2 && ball.nextY-ballSize/2<=wall.y+wallW/2);
+                        }
+                    }
+                });
+            });
         }
     } catch (error) {
         console.log(error);
@@ -235,7 +268,6 @@ window.addEventListener('mousemove', (e) => {
         accelerationY = gravity * Math.sin((rotationX / 180) * Math.PI);
         frictionX = gravity * Math.cos((rotationY / 180) * Math.PI) * friction;
         frictionY = gravity * Math.cos((rotationX / 180) * Math.PI) * friction;
-        window.requestAnimationFrame(main);
     }
 });
 window.addEventListener('keydown', (e) => {
